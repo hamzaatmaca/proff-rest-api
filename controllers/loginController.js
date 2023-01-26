7d61fcdead1b0cbb65a20a8a552edab98a394af1
hamzaatmaca <mentalkod09@gmail.com>
2023-01-26 03:24:32 +0300

    3. push

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,39 +1,49 @@
 const db = require("../database/database");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 module.exports = async (req, res) => {
   await db("users", { userEmail: req.body.email }, "findOne")
     .then((response) => {
-      console.log(response);
-      if (response === null) {
-        res.status(400).json({
-          error: "User Not Found",
+      bcrypt
+        .compare(req.body.password, response.userPassword)
+        .then(function (result) {
+          if (result === true) {
+            if (response === null) {
+              res.status(400).json({
+                error: "User Not Found",
+              });
+            } else {
+              var token = jwt.sign(
+                { email: req.body.email, name: req.body.name },
+                process.env.SECRET
+              );
+
+              const data = {
+                userName: req.body.name,
+                userEmail: req.body.email,
+                token: token,
+              };
+
+              db("login", data, "insertOne")
+                .then(() => {
+                  res.status(201).json({
+                    data,
+                  });
+                })
+                .catch((error) => {
+                  console.log(error);
+                  res.status(400).json({
+                    error: error,
+                  });
+                });
+            }
+          } else {
+            res.status(403).json({
+              error: "Password not valid",
+            });
+          }
         });
-      } else {
-        var token = jwt.sign(
-          { email: req.body.email, name: req.body.name },
-          process.env.SECRET
-        );
-
-        const data = {
-          userName: req.body.name,
-          userEmail: req.body.email,
-          token: token,
-        };
-
-        db("login", data, "insertOne")
-          .then(() => {
-            res.status(201).json({
-              data,
-            });
-          })
-          .catch((error) => {
-            console.log(error);
-            res.status(400).json({
-              error: error,
-            });
-          });
-      }
     })
     .catch((err) => {
       console.log(err);
